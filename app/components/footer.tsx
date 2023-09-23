b0a6fb512524548afa1d9bbb6ead7d02277a5172
Jimii <jimmyimpulse2@gmail.com>
2023-09-23 22:00:53 +0300

    feat: finish layout with footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,10 +1,36 @@
 import { Box } from "@mantine/core";
+import Link from "next/link";
 
 export default function Footer() {
 
+    let footerLinks = [
+        {
+            link: "#",
+            label: "Terms & Conditions",
+        },
+        {
+            link: "#",
+            label: "Privacy",
+        },
+        {
+            link: "#",
+            label: "Collection Statement",
+        },
+        {
+            link: "#",
+            label: `Soundwork © ${new Date().getFullYear()}`,
+        },
+    ];
     return (
-        <Box>
-
+        <Box className="border-t-2 py-3">
+            <Box className="flex justify-center wrap">
+                {footerLinks.map((data, index) => (
+                    <Link href={data.link} className="my-auto hover:cursor-pointer" key={index}>
+                        {data.label}
+                        {index < footerLinks.length - 1 && <span className="my-auto mx-[1vw]">|</span>}
+                    </Link>
+                ))}
+            </Box>
         </Box>
-    )
+    );
 }
