150d85106e37dde42b3a48549e84c079e975cd20
Jimii <jimmyimpulse2@gmail.com>
2023-10-04 00:11:51 +0300

    dev: pre-commit setup

--- services/NFT.ts
+++ services/NFT.ts
@@ -3,24 +3,24 @@ const baseUrl = "";
 /** generate uris and mint tx to be signed on the frontend */
 /*  returns a serialized transaction we need to sign */
 export async function mintSingle(postData: FormData) {
-  const opts = {
-    method: "POST",
-    body: postData,
-  };
+    const opts = {
+        method: "POST",
+        body: postData
+    };
 
-  try {
-    const response = await fetch(
-      "http://localhost:3040/api/v1/nfts/create",
-      opts,
-    );
+    try {
+        const response = await fetch(
+            "http://localhost:3040/api/v1/nfts/create",
+            opts
+        );
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const resp = await response.json();
+        return resp;
+    } catch (err: unknown) {
+        return err;
     }
-
-    const resp = await response.json();
-    return resp;
-  } catch (err: unknown) {
-    return err;
-  }
 }
--- theme.ts
+++ theme.ts
@@ -2,44 +2,44 @@
 
 // variables
 export const resolver: CSSVariablesResolver = (theme) => ({
-  variables: {},
-  light: {
-    "--soundwork-hover-one": "rgb(241 245 249 / 1)",
-    "--soundwork-hover-gradient": "",
-  },
-  dark: {
-    "--soundwork-hover-one": "#34354a",
-    "--soundwork-hover-gradient": "",
-  },
+    variables: {},
+    light: {
+        "--soundwork-hover-one": "rgb(241 245 249 / 1)",
+        "--soundwork-hover-gradient": ""
+    },
+    dark: {
+        "--soundwork-hover-one": "#34354a",
+        "--soundwork-hover-gradient": ""
+    }
 });
 
 import { Button, rem, createTheme, CSSVariablesResolver } from "@mantine/core";
 
 export const theme = createTheme({
-  components: {
-    Button: Button.extend({
-      vars: (theme, props) => {
-        if (props.variant === "primary") {
-          return {
-            root: {
-              "--button-radius": "0px",
-              "--button-bd": "none",
-              "--button-bg":
-                "linear-gradient(90deg, rgba(0, 145, 215, 1), rgba(254, 15, 212, 1))",
-              // "--button-hover": "2px solid white"
-            },
-          };
-        }
+    components: {
+        Button: Button.extend({
+            vars: (theme, props) => {
+                if (props.variant === "primary") {
+                    return {
+                        root: {
+                            "--button-radius": "0px",
+                            "--button-bd": "none",
+                            "--button-bg":
+                                "linear-gradient(90deg, rgba(0, 145, 215, 1), rgba(254, 15, 212, 1))"
+                            // "--button-hover": "2px solid white"
+                        }
+                    };
+                }
 
-        // if (props.size === 'primaryBorder') {
-        //   return {
-        //     root: {
-        //       '--button-radius': '0px',
-        //     },
-        //   };
-        // }
-        return { root: {} };
-      },
-    }),
-  },
+                // if (props.size === 'primaryBorder') {
+                //   return {
+                //     root: {
+                //       '--button-radius': '0px',
+                //     },
+                //   };
+                // }
+                return { root: {} };
+            }
+        })
+    }
 });
