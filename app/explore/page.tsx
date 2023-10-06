7176b8d005b2133d9614eaa3d42d900941c8af6d
Geoffrey Kasuba Sichali <Kasubasich@gmail.com>
2023-10-06 17:47:39 +0200

    making it a bit more

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,39 +1,45 @@
 "use client";
-import { Box, TextInput, Flex, Tabs } from "@mantine/core";
-import SortBy from "../components/Sort";
+import { Box, TextInput, Flex, Tabs, Text, Grid } from "@mantine/core";
 import Cards from "./components/Card";
-import { useState } from "react";
 
 // import AudioPlayer from "./components/AudioPlayer";
 import AudioPlayer from "./components/LocalAudioPlayer";
 
 export default function Explore() {
-  const audioUrl = "/sunella.mp3"; // Adjust the URL based on your Netlify function route
-  // const audioUrl = ".netlify/functions/audio"; // Adjust the URL based on your Netlify function route
+    const audioUrl = "/sunella.mp3"; // To be adjust to call the URL based on the Netlify function route
 
-  return (
-    <div>
-      <Box>
-        <Flex>
-          explore page
-          <TextInput placeholder="Search by collection, music or creators..." />
-          <Box>Sort by</Box>
-        </Flex>
-      </Box>
+    return (
+        <div className="p-5">
+            {/* Header Section */}
+            <Box className="p-5">
+                <div className="flex justify-between items-center">
+                    <Text className="text-lg font-semibold">Explore</Text>
+                    <div className="flex-1 ml-8">
+                        <TextInput
+                            className="w-full p-2 rounded-md border border-gray-300"
+                            placeholder="Search by collection, music, or creators..."
+                        />
+                    </div>
+                    <Box className="ml-4">Sort by</Box>
+                </div>
+            </Box>
 
-      <Box>
-        <AudioPlayer audioUrl={audioUrl} />
-      </Box>
-      <Box>
-        <div>Sound Packs</div>
-        <Box>
-          <Flex>
-            <Tabs defaultValue={"pack"} c={"blue"}>
-              {Cards()}
-            </Tabs>
-          </Flex>
-        </Box>
-      </Box>
-    </div>
-  );
+            {/* Audio Player */}
+            <Box className="my-8">
+                <AudioPlayer audioUrl={audioUrl} />
+            </Box>
+
+            {/* Sound Packs */}
+            <Box>
+                <div className="text-xl font-semibold mb-4">Sound Packs</div>
+                <Box>
+                    <Flex>
+                        <Tabs defaultValue="pack" color="blue">
+                            {Cards()}
+                        </Tabs>
+                    </Flex>
+                </Box>
+            </Box>
+        </div>
+    );
 }
